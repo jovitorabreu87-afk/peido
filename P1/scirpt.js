79f54309035f0f458d68c4428d08cdc92a26313b
Joao Abreu <jovitorabreu87@gmail.com>
2026-06-05 22:14:00 -0300

    new commit

--- P1/scirpt.js
+++ P1/scirpt.js
@@ -1,14 +1,24 @@
-function login(){
-    const user = document.getElementById("nome").value
-    const pass = document.getElementById("senha").value
-        const corretoN = "admin"
-        const corretoS = "123"
-        if(user === corretoN && pass === corretoS ){
-              localStorage.setItem("loged","true")
-               window.location.href = "jgs.html"    
-            }else if(user.length == 0 || pass.length == 0){
-                document.getElementById("err").innerHTML = ("Preencha todas as areas de login!!")
-            }else {
-            document.getElementById("err").innerHTML = ("Senha ou usuario incorreto, tente novamente!")        
-            }
+function cadastrar() {
+    let usuario = document.getElementById("nome").value;
+    let senha = document.getElementById("senha").value;
+
+    localStorage.setItem("nome", usuario);
+    localStorage.setItem("senha", senha);
+
+    alert("Cadastro realizado!");
+    window.location.href = "login.html";
+}
+
+function login() {
+    let usuario = document.getElementById("nome").value;
+    let senha = document.getElementById("senha").value;
+
+    let usuarioSalvo = localStorage.getItem("nome");
+    let senhaSalva = localStorage.getItem("senha");
+
+    if(usuario === usuarioSalvo && senha === senhaSalva){
+        window.location.href = "jgs.html";
+    }else{
+        alert("Usuário ou senha incorretos");
+    }
 }
